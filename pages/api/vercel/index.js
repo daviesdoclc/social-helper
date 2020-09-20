e0f02aeb066fa73feaec8cb29ea69241f095ed55
Davies,Douglas <daviesd@oclc.org>
2020-09-20 11:52:52 -0400

    testing

--- pages/api/vercel/index.js
+++ pages/api/vercel/index.js
@@ -1,19 +1,16 @@
-import exec from 'await-exec'
+import request from 'async-request'
 
 export default async(req, res) => {
 
     const url = 'https://worldcat-org.now.sh/en/title/70775700'
 
-    let {stdout:cookie} = await exec('curl -s -D - -o /dev/null -X POST -d \'_vercel_password=W3lcom3!\' ' + url + ' | grep -i Set-Cookie | grep _vercel_jwt | awk {\'print $2\'}')
+    let response = await request(url, { method: 'POST', data: { "_vercel_password" : "W3lcom3!" } })
 
-    cookie = cookie.replace(';\n', '')
+    let cookie = response.headers['set-cookie'][0]
+    cookie = cookie.substring(0, cookie.indexOf(';'))
 
-    console.log("cookie: ", cookie)
-
-    const {stdout} = await exec('curl -H \'Cookie: ' + cookie + '\' ' + url)
-
-    console.log("stdout: ", stdout)
+    response = await request(url, { headers: { 'Cookie': cookie } })
 
     res.statusCode = 200
-    res.end(stdout)
+    res.end(response.body)
 }
